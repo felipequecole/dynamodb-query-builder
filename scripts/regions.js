03d4fe825d3e90271bf066d9c244c60cac6fa826
Felipe Quécole <felipequecole@msn.com>
2022-01-02 21:49:24 -0300

    Adding regions, handling incognito with form persister and removing commented code

---
+++ scripts/regions.js
@@ -0,0 +1,98 @@
+const regions = [
+    {
+        "Region Name": "US East (Ohio)",
+        "Code": "us-east-2"
+    },
+    {
+        "Region Name": "US East (N. Virginia)",
+        "Code": "us-east-1"
+    },
+    {
+        "Region Name": "US West (N. California)",
+        "Code": "us-west-1"
+    },
+    {
+        "Region Name": "US West (Oregon)",
+        "Code": "us-west-2"
+    },
+    {
+        "Region Name": "Africa (Cape Town)",
+        "Code": "af-south-1"
+    },
+    {
+        "Region Name": "Asia Pacific (Hong Kong)",
+        "Code": "ap-east-1"
+    },
+    {
+        "Region Name": "Asia Pacific (Jakarta)",
+        "Code": "ap-southeast-3"
+    },
+    {
+        "Region Name": "Asia Pacific (Mumbai)",
+        "Code": "ap-south-1"
+    },
+    {
+        "Region Name": "Asia Pacific (Osaka)",
+        "Code": "ap-northeast-3"
+    },
+    {
+        "Region Name": "Asia Pacific (Seoul)",
+        "Code": "ap-northeast-2"
+    },
+    {
+        "Region Name": "Asia Pacific (Singapore)",
+        "Code": "ap-southeast-1"
+    },
+    {
+        "Region Name": "Asia Pacific (Sydney)",
+        "Code": "ap-southeast-2"
+    },
+    {
+        "Region Name": "Asia Pacific (Tokyo)",
+        "Code": "ap-northeast-1"
+    },
+    {
+        "Region Name": "Canada (Central)",
+        "Code": "ca-central-1"
+    },
+    {
+        "Region Name": "China (Beijing)",
+        "Code": "cn-north-1"
+    },
+    {
+        "Region Name": "China (Ningxia)",
+        "Code": "cn-northwest-1"
+    },
+    {
+        "Region Name": "Europe (Frankfurt)",
+        "Code": "eu-central-1"
+    },
+    {
+        "Region Name": "Europe (Ireland)",
+        "Code": "eu-west-1"
+    },
+    {
+        "Region Name": "Europe (London)",
+        "Code": "eu-west-2"
+    },
+    {
+        "Region Name": "Europe (Milan)",
+        "Code": "eu-south-1"
+    },
+    {
+        "Region Name": "Europe (Paris)",
+        "Code": "eu-west-3"
+    },
+    {
+        "Region Name": "Europe (Stockholm)",
+        "Code": "eu-north-1"
+    },
+    {
+        "Region Name": "Middle East (Bahrain)",
+        "Code": "me-south-1"
+    },
+    {
+        "Region Name": "South America (São Paulo)",
+        "Code": "sa-east-1"
+    }
+];
